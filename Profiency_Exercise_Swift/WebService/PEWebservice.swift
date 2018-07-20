52b6cd8961c2b2b6f6863eadc42657feacf0ffe3
sabapathy7 <kanagasabapathy@farshore.com>
2018-07-20 12:45:14 +0530

    Web service connection

--- Profiency_Exercise_Swift/WebService/PEWebservice.swift
+++ Profiency_Exercise_Swift/WebService/PEWebservice.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+// MARK: List of Constants
+let API_COUNTRY:String = "a_string_constant"
+
+
+class PEWebservice{
+   
+    func parseJSON(){
+        
+        let url = URL(string: API_COUNTRY)
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            guard error == nil else{
+                print("returned error")
+                return
+            }
+            guard let content = data else{
+                print("No data")
+                return
+            }
+            guard let json = (try?JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers))as? [String:Any] else{
+                print("No JSON data from response")
+                return
+            }
+            if let array = json["rows"] as? [String:Any]{
+                
+            }
+        }
+        
+    
+    }
+}
